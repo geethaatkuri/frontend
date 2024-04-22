1a4752ee49a6f64db6a7e0073b7a7c114f46e4d5
Geetha <atkurigeetha910@gmail.com>
2024-04-22 20:44:23 +0530

    Adding JavaScript Learnt topics and a Responsive website of University Project

--- JavaScript/Arrays.js
+++ JavaScript/Arrays.js
@@ -17,27 +17,65 @@
 //console.log(words[2]); //Accessing the value at 2th position using index
 //console.log(words[70]); //undefined. Since there is no value and position of 70th in the array list
 // console.log(words[5]);
-//console.log(words.length);
+//console.log(words.length); //Finding length
+//console.log(typeof words);
 // const newWords = words;
 // newWords[6] = 'Hi';
 // console.log(words);
 // console.log(newWords);
 
 
+//Using for loop in array
+
+let marks = [55, 66, 88, 98, 55, 78];
+for (let i = 0; i < marks.length; i++) {
+  console.log(marks[i]);
+}
 
 
 
+//Array Methods
 
-const nums = [4, 1, 2, 8, 2, 8, 6];
-console.log(nums.indexOf(8)); //at which postion num is present & prints the first position. If number is not present then returns -1
-console.log(nums.includes(8));
+// let nums = [4, 1, 2, 8, 2, 8, 6];
+// let B = nums.toString()//b is now a string
+// console.log(B, typeof B);
+// let C = nums.join("_") //joins all the array elements using a separator
+// console.log(C, typeof C);
+//console.log(nums.indexOf(8)); //at which postion num is present & prints the first position. If number is not present then returns -1
+//console.log(nums.includes(8));
+
+// let nums = [4, 1, 2, 8, 2, 8, 6];
+// let nums_more = [11, 12, 13, 14];
+// let newArray = nums.concat(nums_more)
+// console.log(newArray);
+
+// let r = nums.pop(); //removes last element from the array
+// console.log(nums, r);
 //nums.push("Pushes the element at the end of array and gives the new length of array.")
 //nums.unshift("Pushes the element into the starting of array i.e., in 0th index")
-//nums.pop(); //removes last element from the array
 //nums.shift(); //removes the first element
-nums.sort();
-const subArray = nums.slice(2,5);
-console.log(subArray);
-console.log(nums);
+// nums.sort(); //Modifies the original array
+// const subArray = nums.slice(2,5);
+// console.log(subArray);
+// console.log(nums);
 
+
+//Splice and slice
+
+//Splice can be used to add new items into the array
+// let nums = [4, 1, 2, 8, 2, 8, 6, 7];
+// nums.splice(2, 3, 1022, 1023, 1024) // 2--> position. 3-->from strting position number 2, 3 elements are removed. 1022,1023,1024-->these elements are placed in the removed elements.
+// console.log(nums);
+
+//Slice --> slices out a piece from an array. It creates new array.
+// let newnums = nums.slice(3,7)
+// console.log(newnums);
+
+
+
+
+
+//Looping through arrays--> for loop, for-in, for-each, for-
+
+let nums = [4, 1, 2, 8, 2, 8, 6, 7];
 
--- JavaScript/FunctionsAdvanced.js
+++ JavaScript/FunctionsAdvanced.js
@@ -1,55 +1,126 @@
 //This keyword
-const user = {
-    username: 'Geetha',
-    price: 1000,
+// const user = {
+//     username: 'Geetha',
+//     price: 1000,
 
-    welcomeMessage: function() {
-        console.log(`Hello ${this.username}, welcome to the website`);
-    } 
+//     welcomeMessage: function() {
+//         console.log(`Hello ${this.username}, welcome to the website`);
+//     } 
+// }
+// user.welcomeMessage()
+
+
+// //Arrow function
+
+// const greet = (count) => {
+//     for(let i = 0; i < count; i++)
+//     console.log("Hello world");
+// };
+
+// greet(3);
+
+
+// const square = (num) => num * num;
+// console.log(square(3));
+
+// //Callback function
+
+// const calculate = (a,b, operation) => {
+//     return operation(a,b);
+// };
+
+
+// //Method-1
+// const addition = calculate(3,4, function (num1, num2){
+//     return num1 + num2;
+// })
+// console.log(addition);
+
+
+// //Method-2
+// const subtraction = (a,b) => a-b;
+
+// const subResult = calculate(8, 5, subtraction);
+
+// console.log(subResult);
+
+
+// //Method-3
+// function multiply(a,b){
+//     return a * b;
+// }
+
+// const mulResult = calculate(3,2, multiply);
+// console.log(mulResult);
+
+
+//setTimeout function
+// console.log('Hello');
+// console.log('World');
+// setTimeout(function(){
+//     console.log("This will execute 4 secs later");
+// }, 4000);
+// console.log("Four Seconds");
+// console.log("Time has been set");
+
+
+//Making pizza using callback function example.
+function getCheese(callback) {
+    setTimeout(() =>{
+        const cheese = "🧀";
+        console.log("Here is the cheese ", cheese);
+        callback(cheese);
+    }, 2000);
 }
-user.welcomeMessage()
 
-
-//Arrow function
-
-const greet = (count) => {
-    for(let i = 0; i < count; i++)
-    console.log("Hello world");
-};
-
-greet(3);
-
-
-const square = (num) => num * num;
-console.log(square(3));
-
-//Callback function
-
-const calculate = (a,b, operation) => {
-    return operation(a,b);
-};
-
-
-//Method-1
-const addition = calculate(3,4, function (num1, num2){
-    return num1 + num2;
-})
-console.log(addition);
-
-
-//Method-2
-const subtraction = (a,b) => a-b;
-
-const subResult = calculate(8, 5, subtraction);
-
-console.log(subResult);
-
-
-//Method-3
-function multiply(a,b){
-    return a * b;
+function makeDough(cheese, callback) {
+    setTimeout( () => {
+        const dough = cheese + '🍥';
+        console.log('Here is the dough', dough);
+        callback(dough);
+    }, 2000);
 }
 
-const mulResult = calculate(3,2, multiply);
-console.log(mulResult);
+function bakePizza(dough, callback) {
+    setTimeout( () => {
+        const pizza = dough + '🍕';
+        console.log('Here is the pizza', pizza);
+        callback(pizza);
+    }, 2000);
+}
 
+// console.log(getCheese());
+getCheese((cheese) => {
+    // console.log("Got the cheese", cheese);
+    makeDough(cheese, (dough) => {
+        bakePizza(dough, (pizza) => {
+            console.log("Got my pizza", pizza);
+        });
+        // console.log("Got my dough", dough);
+    });
+});
+
+
+
+//Array Callback Function
+// const a = [4, 1, 6, -2, -5, 3, 2, -8, 6, 7];
+//const firstNeg = (num) => {
+  //  return num < 0;
+//}
+
+//const result = a.find(firstNeg);
+//const result = a.findIndex(firstNeg);
+
+//console.log(result);
+
+// a.forEach((num, i) => {
+//     console.log('array num', num, i);
+// });
+
+
+
+//Write a function prgm to find mean of 5 numbers.
+const mean = (a,b,c,d) => {
+    return (a+ b+ c+ d) / 4 
+}
+console.log(mean(4, 5, 6, 7));
